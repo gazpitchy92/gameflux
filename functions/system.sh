14b1c352d14a8bb21ad5c9ee2d83d6f730dd9005
garethpitchfordLF <gareth.pitchford@lowe-and-fletcher.co.uk>
2025-09-18 02:32:38 +0100

    remove unused cpu feature

--- functions/system.sh
+++ functions/system.sh
@@ -3,15 +3,3 @@
 clear_ram_buffer() {
     echo 3 | sudo tee /proc/sys/vm/drop_caches
 }
-
-update_power() {
-    if [ "$1" = "gaming" ]; then
-        sudo cpupower frequency-set --governor performance
-        sudo cpupower frequency-set --max 5400000
-        sudo cpupower frequency-set --min 4700000
-    elif [ "$1" = "general" ]; then
-        sudo cpupower frequency-set --governor schedutil
-        sudo cpupower frequency-set --max 5200000
-        sudo cpupower frequency-set --min 2990000
-    fi
-}
